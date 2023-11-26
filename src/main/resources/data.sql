c1f63489b5c78b87c59a33e3cb3cd9f8c7d5f476
DryRains <dryrain_@naver.com>
2023-11-26 17:15:20 +0900

    setup: initial data settings

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Owner (id) VALUES (1);
+INSERT INTO Restaurant (id, owner_id, name, category, phone_number, address, status, opening_days, opening_hours, closing_hours)
+VALUES (2, 1, 'KenStaurant', '일식', '02-123-1234', '서울특별시 서대문구 남가좌동', 'ACTIVE', '월,화,수,목,금', '09:00', '20:00');
